8e39cbe77b04fc140090aa82cf95721cd4a94eb0
Hayato_Endo <58716265+hayato78-ai@users.noreply.github.com>
2021-05-15 09:21:39 +0900

    0回目課題

---
+++ 0回目課題/html_Endo_44/js/main.js
@@ -0,0 +1,26 @@
+// jsを記述する際はここに記載していく
+$(function(){
+	$(window).scroll(function (){
+		$('.fadein').each(function(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight + 250){
+				$(this).addClass('scrollin');
+			}
+		});
+	});
+});
+
+$(function(){
+    　$(window).scroll(function (){
+        $('.effect-fade').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight){
+                $(this).addClass('effect-scroll');
+            }
+        });
+    　});
+    });
